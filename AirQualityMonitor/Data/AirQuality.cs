335d64f569c0d61ca33a2a46412c213f3b275edd
Gokulnathan Perumal <gokulnathanpeee@gmail.com>
2023-05-18 16:00:15 +0530

    Code Optimization

--- AirQualityMonitor/Data/AirQuality.cs
+++ AirQualityMonitor/Data/AirQuality.cs
@@ -23,24 +23,21 @@ public class AirQualityDocument
     public string Name { get; set; }
     public Fields Fields { get; set; }
     public DateTime CreateTime { get; set; }
-    public DateTime UpdateTime { get; set; }
+	public DateTime UpdateTime { get; set; }
+    public long DocumentId => Int64.Parse(Name?.Split('/').Last());
 }
 
 public class Fields
 {
     public Temperature Temperature { get; set; }
-
     public CreatedDate CreatedDate { get; set; }
 }
 
 public class Temperature
 {
     public double DoubleValue { get; set; }
-
     public string Celsius => DoubleValue.ToString("0.00°C");
-
     public string Fahrenheit => (32 + (DoubleValue / 0.5556)).ToString("0.00°F");
-
     public string Summary => new Dictionary<int, string>() { { 1, "Freezing" }, { 2, "Bracing" }, { 3, "Chilly" }, { 5, "Mild" }, { 6, "Warm" }, { 7, "Balmy" }, { 8, "Hot" }, { 9, "Sweltering" }, { 10, "Scorching" } }[Math.Max(1, Math.Min((int)(DoubleValue / 5), 10))];
 }
 
